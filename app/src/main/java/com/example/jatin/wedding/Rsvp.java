b1df284b2721c8fe8375a2d0528f53d22521d3c6
parmindersinghsethi94 <parmindersinghsethi94@gmail.com>
2015-07-14 18:17:27 +0530

    RSVP done

--- app/src/main/java/com/example/jatin/wedding/Rsvp.java
+++ app/src/main/java/com/example/jatin/wedding/Rsvp.java
@@ -1,7 +1,6 @@
 package com.example.jatin.wedding;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
@@ -10,44 +9,56 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.*;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends ActionBarActivity {
+import static android.app.PendingIntent.getActivity;
+
+
+public class Rsvp extends ActionBarActivity {
+    Spinner spinner;
     Button btn;
-    EditText phone;
-    String phoneNumber;
     String TAG="check";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        phone= (EditText) findViewById(R.id.editText);
-        btn= (Button) findViewById(R.id.button);
-
+        setContentView(R.layout.activity_rsvp);
+        spinner = (Spinner) findViewById(R.id.spinner);
         final ProgressDialog pDialog = new ProgressDialog(this);
+        SharedPreferences preferences= getSharedPreferences("Phone", MODE_PRIVATE);
+        final String userPhone=preferences.getString("user",null);
+        List<String> list = new ArrayList<String>();
+        for(int i=1;i<101;i++) {
+            list.add(i+"");
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+        btn= (Button) findViewById(R.id.submit);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                phoneNumber=phone.getText().toString();
-                // Tag used to cancel the request
+                String a= (String) spinner.getSelectedItem();
+                Toast.makeText(getApplicationContext(),"You are coming with "+a+" more.",Toast.LENGTH_LONG).show();
                 String tag_json_obj = "json_obj_req";
-                String url = "http://10.10.20.169:82/newTrend/login.php?number="+phoneNumber;
-                pDialog.setMessage("Loading...");
+                String url = "http://10.10.20.169:82/newTrend/rsvp.php?rsvp="+a+"&user="+userPhone;
+                pDialog.setMessage("Sending the request...");
                 pDialog.show();
-
                 JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
                         url, null,
                         new Response.Listener<JSONObject>() {
@@ -59,18 +70,13 @@ public class MainActivity extends ActionBarActivity {
                                     JSONObject json=dataSet.getJSONObject(0);
                                     String check= (String) json.get("check");
                                     if(check.equals("yes")){
-                                        //number is registered
-                                        SharedPreferences sharedPreferences = getSharedPreferences("Phone", Context.MODE_PRIVATE);
-                                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                                        editor.putString("user",phoneNumber);
-                                        editor.commit();
-                                        Intent intent =new Intent(MainActivity.this,Rsvp.class);
-                                        startActivity(intent);
+                                        //done
+
 
                                     }
                                     else{
-                                        //number is not registered
-                                        Toast.makeText(getApplicationContext(),"Your number is not registered",Toast.LENGTH_LONG).show();
+                                        //not done
+                                        Toast.makeText(getApplicationContext(),"Something went wrong,Please try again !!",Toast.LENGTH_LONG).show();
                                     }
 
 
@@ -91,7 +97,6 @@ public class MainActivity extends ActionBarActivity {
                     }
                 });
 
-
 // Adding request to request queue
                 AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
 
@@ -100,11 +105,10 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_rsvp, menu);
         return true;
     }
 
